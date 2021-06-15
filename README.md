f0327fb69fa69fd99141cf3065d5abdcd1cd1ed7
ashill99 <admshng@gmail.com>
2021-06-15 22:44:31 +0100

    ducking & jumping

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 high score 725 - hit by flying bird
-high score 782 - hit by cactus after ducking and not getting up 
+high score 906 - hit by cactus after ducking and not getting up 
+high score 1155 - ducking and jumping!
 
 console logged objects to check its position and added if statement to stop that. 
--- dino.js
+++ dino.js
@@ -9,29 +9,36 @@ const game = Runner.instance_;
 	let tRex = game.tRex;
 
     if(obstacles.length) {
+        
     let obstacle = obstacles[0];
     let tRexWidth = tRex.config.WIDTH;
-	let jump = document.dispatchEvent(new KeyboardEvent("keydown", {keyCode: 38}));
-    let duck = document.dispatchEvent(new KeyboardEvent("keydown", {keyCode: 40}));
+
+	const jump = () => {
+        document.dispatchEvent(new KeyboardEvent("keydown", {keyCode: 38}));
+    };
+
+    const duck = () => {
+         document.dispatchEvent(new KeyboardEvent("keydown", {keyCode: 40}));
+         setTimeout(function() {
+            document.dispatchEvent(new KeyboardEvent("keyup", {keyCode: 40}));
+         },300);
+    };
 
     if (obstacle.xPos - tRex.xPos - tRexWidth <= 20 && obstacle.yPos > 85) {
         if (!tRex.jumping) {
             console.log("jump");
             console.log(obstacle);
-            jump  ;     
+            jump();     
         }
-    }
-
-    while (obstacle.xPos - tRex.xPos - tRexWidth <= 20 && obstacle.yPos < 85) {
+    } else if (obstacle.xPos - tRex.xPos - tRexWidth <= 20 && obstacle.yPos < 85) {
         if (!tRex.ducking) {
             console.log("duck");
             console.log(obstacle);
-            duck;
+            duck();
             console.log("get up")
-            setTimeout(jump, 200);
         }
     }
-}
+};
 
   requestAnimationFrame(tick);
 }());
